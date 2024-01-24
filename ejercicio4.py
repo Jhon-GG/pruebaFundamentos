0a2686c79bff7584931965dd434be35d8ffebd59
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-01-24 12:28:18 -0500

    Prueba Fundamentos Ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+dist = float(input("Ingrese el valor de la distancia en metros:  \n"))
+
+op = (dist * 1) / 1000
+
+print(f"La distancia {dist} en kilometros es:  {op} kilómetros.")
+
+
+
+# 1000m - 1km
+#  var -  x
